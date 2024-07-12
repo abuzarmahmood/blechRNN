4330711ed719f095e5f267f2478ed388c09ea339
Abuzar Mahmood <abuzarmahmood@gmail.com>
2024-07-12 11:32:43 -0400

    Clean-up model running script

--- src/run_model.py
+++ src/run_model.py
@@ -26,9 +26,6 @@ dat.get_spikes()
 dat.get_firing_rates()
 
 spike_array = np.stack(dat.spikes)
-# Drop first n trials
-spike_n_trials = 0
-spike_array = spike_array[:,spike_n_trials:]
 
 cat_spikes = np.concatenate(spike_array)
 
@@ -36,9 +33,6 @@ cat_spikes = np.concatenate(spike_array)
 bin_size = 25
 binned_spikes = np.reshape(cat_spikes, 
                            (*cat_spikes.shape[:2], -1, bin_size)).sum(-1)
-
-# vz.firing_overview(binned_spikes.swapaxes(0,1))
-# plt.show()
 
 # Reshape to (seq_len, batch, input_size)
 inputs = binned_spikes.copy()
@@ -69,10 +63,6 @@ inputs_trial_pca = inputs_pca.reshape(inputs.shape[0], -1, n_components)
 inputs = inputs_trial_pca.copy()
 
 ##############################
-
-# Add taste number as external input
-taste_number = np.repeat(np.arange(4), inputs.shape[1]//4)
-taste_number = np.broadcast_to(taste_number, (inputs.shape[0], inputs.shape[1]))
 
 # Add stim time as external input
 stim_time = np.zeros_like(taste_number)
@@ -124,67 +114,55 @@ train_labels = train_labels.to(device)
 test_inputs = test_inputs.to(device)
 test_labels = test_labels.to(device)
 
-hidden_size_vec = [6, 8]
-loss_funcs_vec = ['mse']
-repeats = 3
+##############################
+# Train 
+##############################
+hidden_size = 8
+loss = 'mse'
 
-param_product = [[[x,y]]*repeats for x in hidden_size_vec for y in loss_funcs_vec]
-param_product = [item for sublist in param_product for item in sublist]
+net = autoencoderRNN( 
+        input_size=input_size,
+        hidden_size= hidden_size, 
+        output_size=output_size,
+        dropout = 0.2,
+        )
+net.to(device)
+net, loss, cross_val_loss = train_model(
+        net, 
+        train_inputs, 
+        train_labels, 
+        lr = 0.001, 
+        train_steps = 15000,
+        loss = loss, 
+        test_inputs = test_inputs,
+        test_labels = test_labels,
+        )
 
-# n_restarts = len(hidden_size_vec)
-model_list = []
-loss_list = []
-cross_val_loss_list = []
-# for i in trange(n_restarts):
-for i, this_params in enumerate(tqdm(param_product)):
-    net = autoencoderRNN( 
-            input_size=input_size,
-            hidden_size= this_params[0], 
-            output_size=output_size,
-            dropout = 0.2,
-            # rnn_layers = 3,
-            )
-    net.to(device)
-    net, loss, cross_val_loss = train_model(
-            net, 
-            train_inputs, 
-            train_labels, 
-            lr = 0.001, 
-            train_steps = 15000,
-            loss = this_params[1], 
-            test_inputs = test_inputs,
-            test_labels = test_labels,
-            )
-    model_list.append(net)
-    loss_list.append(loss)
-    cross_val_loss_list.append(cross_val_loss)
+model_name = f'hidden_{hidden_size}_loss_{loss}'
+torch.save(net, os.path.join(artifacts_dir, f'{model_name}.pt'))
 
-    model_name = f'hidden_{this_params[0]}_loss_{this_params[1]}_{i}'
-    torch.save(net, os.path.join(artifacts_dir, f'{model_name}.pt'))
+# Make plots
+fig, ax = plt.subplots()
+for ind, loss in enumerate(loss_list):
+    ax.plot(loss, label = model_name) 
+ax.legend(
+        bbox_to_anchor=(1.05, 1), 
+        loc='upper left', borderaxespad=0.)
+ax.set_title(f'Losses') 
+fig.savefig(os.path.join(plot_dir,'run_loss.png'),
+            bbox_inches = 'tight')
+plt.close(fig)
 
-    fig, ax = plt.subplots()
-    # ax.plot(np.stack(loss_list).T, alpha = 0.5)
-    # ax.set_title(f'Losses for {i} restarts')
-    for ind, loss in enumerate(loss_list):
-        ax.plot(loss, label = f'params: {param_product[ind]}') 
-    ax.legend(
-            bbox_to_anchor=(1.05, 1), 
-            loc='upper left', borderaxespad=0.)
-    ax.set_title(f'Losses for {i+1}/{len(param_product)} restarts')
-    fig.savefig(os.path.join(plot_dir,'run_loss.png'),
-                bbox_inches = 'tight')
-    plt.close(fig)
-
-    # Plot another figure of cross_val_loss
-    fig, ax = plt.subplots()
-    for ind, loss in enumerate(cross_val_loss_list):
-        ax.plot(loss.keys(), loss.values(),
-                label = f'params: {param_product[ind]}')
-    ax.legend(
-            bbox_to_anchor=(1.05, 1),
-            loc='upper left', borderaxespad=0.)
-    ax.set_title(f'Cross Val Losses for {i+1}/{len(param_product)} restarts')
-    fig.savefig(os.path.join(plot_dir,'cross_val_loss.png'),
-                bbox_inches = 'tight')
-    plt.close(fig)
+# Plot another figure of cross_val_loss
+fig, ax = plt.subplots()
+for ind, loss in enumerate(cross_val_loss_list):
+    ax.plot(loss.keys(), loss.values(),
+            label = model_name) 
+ax.legend(
+        bbox_to_anchor=(1.05, 1),
+        loc='upper left', borderaxespad=0.)
+ax.set_title(f'Cross Val Losses') 
+fig.savefig(os.path.join(plot_dir,'cross_val_loss.png'),
+            bbox_inches = 'tight')
+plt.close(fig)
 
